f77487b740bc6a5bbaf2c5cf5117a7ed22e8ef37
VincenzoBorriello89 <vincborr89@gmail.com>
2022-06-09 17:07:07 +0200

    creaz colonna category in index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -15,6 +15,7 @@
             <thead>
                 <tr>
                     <th scope="col">title</th>
+                    <th scope="col">Category</th>
                     <th scope="col">content</th>
                     <th scope="col">image</th>
                     <th scope="col">slug</th>
@@ -25,6 +26,13 @@
                 @forelse ($posts as $post)
                 <tr>
                     <th>{{ $post->title }}</th>
+                    <td>
+                        @if( $post->category )
+                            <span class="badge badge-pill badge-{{$post->Category->color}}">{{ $post->Category->label}}</span>
+                        @else
+                            -
+                        @endif
+                    </td>
                     <td>
                         <p>{{ $post->content }}</p>
                     </td>
