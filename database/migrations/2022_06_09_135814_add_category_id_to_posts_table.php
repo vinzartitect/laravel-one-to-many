fd23040ca6d1ac99465945b88d26b2427e5ffb34
VincenzoBorriello89 <vincborr89@gmail.com>
2022-06-09 16:09:56 +0200

    relazione base tra tabelle fatta

---
+++ database/migrations/2022_06_09_135814_add_category_id_to_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCategoryIdToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //aggiunta colonna alla tabella posts
+            $table->unsignedBigInteger('category_id')->nullable()->after('id');
+
+            //realizzare la relazione
+            $table->foreign('category_id')
+                    ->references('id')
+                    ->on('categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign('posts_category_id_foreign');
+
+            $table->dropColumn('category_id');
+        });
+    }
+}
