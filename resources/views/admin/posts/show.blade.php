1bdb509756e5a1c24b91b6e4dea5af6027eac56e
VincenzoBorriello89 <vincborr89@gmail.com>
2022-06-09 17:40:26 +0200

    visual category nella show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,6 +4,11 @@
 <div class="container">
     <img src="{{ $post->image }}" alt="$post->title" width="50%">
     <h2>{{ $post->title }}</h2>
+    @if( $post->category )
+    <span class="badge badge-pill badge-{{$post->Category->color}}">{{ $post->Category->label}}</span>
+    @else
+    nessuna categoria è assegnata
+    @endif
     <p>{{ $post->content }}</p>
     {{-- <form action="{{route('admin.posts.destroy', $post->id)}}" method="POST" class="delete-form">
         @method('DELETE')
